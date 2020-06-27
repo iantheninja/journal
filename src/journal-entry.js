d9f3a7f8df7c5f109b1bae0229332964a461bf90
iantheninja <munene.ian@live.com>
2020-06-27 23:51:58 +0300

    javascript code files linked

---
+++ src/journal-entry.js
@@ -0,0 +1,3 @@
+export function journalEntry() {
+  console.log("This code fucks!!!");  
+}
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { journalEntry } from './journal-entry';
+import './styles.css';
+
+$(document).ready(function(event) {
+  console.log("pages are connected to one another");
+  console.log(journalEntry());
+    
+});
